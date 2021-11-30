e2d01d9c1d34e00945ef9ddc4d55a164d5c68701
Dimkinsl <dimkinz@gmail.com>
2021-11-30 20:43:51 +0500

    change env

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Старт прграммы в зависимости от подключаемого модуля средства коммуникации
 
-import sys
+import os
 import MessengersAPI.TelegramAPI as TelegramAPI
 import MessengersAPI.FBAPI as FBAPI
 import MessengersAPI.VKAPI as VKAPI
@@ -8,12 +8,12 @@ import MessengersAPI.SkypeAPI as SkypeAPI
 
 # Запускаем прложение через номер средство коммуникации
 if __name__ == '__main__':
-    API = int(sys.argv[1])
-    if API == 0:
+    API = os.environ['Method_API']
+    if API == 'Telegram':
         TelegramAPI.start_programm()
-    elif API == 1:
+    elif API == 'FACEBOOK':
         FBAPI.start_programm()
-    elif API == 2:
+    elif API == 'VK':
         VKAPI.start_programm()
-    elif API == 3:
+    elif API == 'SKYPE':
         SkypeAPI.start_programm()
